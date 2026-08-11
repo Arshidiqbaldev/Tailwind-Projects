f8cc9de8ba3a99bac69161fc1200b5c7b7c96585
Arshidiqbaldev <arshidiqbalnet@gmail.com>
2026-08-11 16:49:20 +0500

    v-004

--- src/myscript.js
+++ src/myscript.js
@@ -18,182 +18,107 @@ document.addEventListener("DOMContentLoaded", () => {
 const mainDiv = document.querySelectorAll(".main");
 
 
+const navItems = [
+    {
+        name: "Home",
+        href: "index.html"
+    },
+    {
+        name: "Line Nav",
+        href: "line-nav.html"
+    },
+    {
+        name: "Animated Spinner",
+        href: "animated-spinner.html"
+    },
+    {
+        name: "Modern Button",
+        href: "modern-button.html"
+    },
+    {
+        name: "Modern Menu",
+        href: "modern-menu.html"
+    },
+    {
+        name: "Cards",
+        href: "cards.html"
+    },
+    {
+        name: "Nav bars",
+        href: "navbars.html"
+    },
+    {
+        name: "Modern page",
+        href: "Modern-page.html"
+    }
+];
+
+
+const navBar = document.createElement("nav");
+
+navBar.className =
+    "flex nav flex-col gap-2 w-72 p-4 justify-center border border-zinc-800 rounded-4xl";
+
+const fragment = document.createDocumentFragment();
+
+navItems.forEach((item, index) => {
+
+    const link = document.createElement("a");
+
+    link.href = item.href;
+
+    link.className =
+        "nav-link group relative flex h-px items-center after:absolute after:size-full after:top-1/2 after:left-0 after:-translate-y-1/2 after:p-3.5 gap-3";
+
+    link.innerHTML = `
+        <span class="
+            nav-line
+            block
+            h-px
+            w-6
+            shrink-0
+            bg-white/20
+            transition-all
+            duration-300
+            ease-out
+            group-hover:w-12
+            group-hover:bg-white
+            group-aria-[current=page]:w-10
+            group-aria-[current=page]:bg-white
+        "></span>
+
+        <span class="
+            text-sm
+            text-white/60
+            transition-colors
+            duration-300
+            group-hover:text-white
+            group-aria-[current=page]:text-white
+        ">
+            ${item.name}
+        </span>
+    `;
+
+    fragment.append(link);
+
+    // Add separators except after the last item
+    if (index < navItems.length - 1) {
+        const separator1 = document.createElement("span");
+        separator1.className = "block h-px w-6 bg-white/20";
+
+        const separator2 = document.createElement("span");
+        separator2.className = "block h-px w-6 bg-white/20";
+
+        fragment.append(separator1, separator2);
+    }
+});
+
+navBar.append(fragment);
+
+
+
+
 
 mainDiv.forEach((main) => {
-
-
-    const navBar = document.createElement("nav");
-    navBar.className = "flex nav flex-col gap-2 w-72 p-4 justify-center  border border-zinc-800 rounded-4xl";
-
-    navBar.innerHTML = `
-
-            <a href="index.html"
-                class="nav-link group relative flex h-px items-center after:absolute after:size-full after:top-1/2 after:left-0 after:-translate-y-1/2  after:p-3.5 gap-3">
-
-                <span class="nav-line block h-px w-6 shrink-0 bg-white/20 transition-all duration-300 ease-out
-                    group-hover:w-12
-                    group-hover:bg-white
-                    group-aria-[current=page]:w-10
-                    group-aria-[current=page]:bg-white">
-                </span>
-
-                <span class="text-sm text-white/60 transition-colors duration-300
-                    group-hover:text-white
-                    group-aria-[current=page]:text-white">
-                    Home
-                </span>
-
-            </a>
-
-            <span class="block h-px w-6 bg-white/20"></span>
-            <span class="block h-px w-6 bg-white/20"></span>
-
-            <a href="line-nav.html"
-                class="nav-link group relative flex h-px items-center after:absolute after:size-full after:top-1/2 after:left-0 after:-translate-y-1/2  after:p-3.5 gap-3">
-
-                <span class="nav-line block h-px w-6 shrink-0 bg-white/20 transition-all duration-300
-                    group-hover:w-12
-                    group-hover:bg-white
-                    group-aria-[current=page]:w-10
-                    group-aria-[current=page]:bg-white">
-                </span>
-
-                <span class="text-sm text-white/60 transition-colors duration-300
-                    group-hover:text-white
-                    group-aria-[current=page]:text-white">
-                    Line Nav
-                </span>
-
-            </a>
-
-            <span class="block h-px w-6 bg-white/20"></span>
-            <span class="block h-px w-6 bg-white/20"></span>
-
-            <a href="animated-spinner.html"
-                class="nav-link group relative flex h-px after:absolute after:size-full after:top-1/2 after:left-0 after:-translate-y-1/2  after:p-3.5 items-center gap-3">
-
-                <span class="nav-line block h-px w-6 shrink-0 bg-white/20 transition-all duration-300
-                    group-hover:w-12
-                    group-hover:bg-white
-                    group-aria-[current=page]:w-10
-                    group-aria-[current=page]:bg-white">
-                </span>
-
-                <span class="text-sm text-white/60 transition-colors duration-300
-                    group-hover:text-white
-                    group-aria-[current=page]:text-white">
-                    Animated Spinner
-                </span>
-
-            </a>
-
-            <span class="block h-px w-6 bg-white/20"></span>
-            <span class="block h-px w-6 bg-white/20"></span>
-
-            <a href="modern-button.html"
-                class="nav-link group relative flex h-px after:absolute after:size-full after:top-1/2 after:left-0 after:-translate-y-1/2  after:p-3.5 items-center gap-3">
-
-                <span class="nav-line block h-px w-6 shrink-0 bg-white/20 transition-all duration-300
-                    group-hover:w-12
-                    group-hover:bg-white
-                    group-aria-[current=page]:w-10
-                    group-aria-[current=page]:bg-white">
-                </span>
-
-                <span class="text-sm text-white/60 transition-colors duration-300
-                    group-hover:text-white
-                    group-aria-[current=page]:text-white">
-                    Modern Button
-                </span>
-
-            </a>
-
-
-            <span class="block h-px w-6 bg-white/20"></span>
-            <span class="block h-px w-6 bg-white/20"></span>
-
-            <a href="modern-menu.html"
-                class="nav-link group relative flex h-px after:absolute after:size-full after:top-1/2 after:left-0 after:-translate-y-1/2  after:p-3.5 items-center gap-3">
-
-                <span class="nav-line block h-px w-6 shrink-0 bg-white/20 transition-all duration-300
-                    group-hover:w-12
-                    group-hover:bg-white
-                    group-aria-[current=page]:w-10
-                    group-aria-[current=page]:bg-white">
-                </span>
-
-                <span class="text-sm text-white/60 transition-colors duration-300
-                    group-hover:text-white
-                    group-aria-[current=page]:text-white">
-                    Modern Menu
-                </span>
-
-            </a>
-
-
-            <span class="block h-px w-6 bg-white/20"></span>
-            <span class="block h-px w-6 bg-white/20"></span>
-
-            <a href="cards.html"
-                class="nav-link group relative flex h-px after:absolute after:size-full after:top-1/2 after:left-0 after:-translate-y-1/2  after:p-3.5 items-center gap-3">
-
-                <span class="nav-line block h-px w-6 shrink-0 bg-white/20 transition-all duration-300
-                    group-hover:w-12
-                    group-hover:bg-white
-                    group-aria-[current=page]:w-10
-                    group-aria-[current=page]:bg-white">
-                </span>
-
-                <span class="text-sm text-white/60 transition-colors duration-300
-                    group-hover:text-white
-                    group-aria-[current=page]:text-white">
-                    Cards
-                </span>
-
-            </a>
-
-            <span class="block h-px w-6 bg-white/20"></span>
-            <span class="block h-px w-6 bg-white/20"></span>
-
-            <a href="navbars.html"
-                class="nav-link group relative flex h-px after:absolute after:size-full after:top-1/2 after:left-0 after:-translate-y-1/2  after:p-3.5 items-center gap-3">
-
-                <span class="nav-line block h-px w-6 shrink-0 bg-white/20 transition-all duration-300
-                    group-hover:w-12
-                    group-hover:bg-white
-                    group-aria-[current=page]:w-10
-                    group-aria-[current=page]:bg-white">
-                </span>
-
-                <span class="text-sm text-white/60 transition-colors duration-300
-                    group-hover:text-white
-                    group-aria-[current=page]:text-white">
-                    Nav bars
-                </span>
-
-            </a>
-
-            <span class="block h-px w-6 bg-white/20"></span>
-            <span class="block h-px w-6 bg-white/20"></span>
-
-            <a href="Modern-page.html"
-                class="nav-link group relative flex h-px after:absolute after:size-full after:top-1/2 after:left-0 after:-translate-y-1/2  after:p-3.5 items-center gap-3">
-
-                <span class="nav-line block h-px w-6 shrink-0 bg-white/20 transition-all duration-300
-                    group-hover:w-12
-                    group-hover:bg-white
-                    group-aria-[current=page]:w-10
-                    group-aria-[current=page]:bg-white">
-                </span>
-
-                <span class="text-sm text-white/60 transition-colors duration-300
-                    group-hover:text-white
-                    group-aria-[current=page]:text-white">
-                    Modern page
-                </span>
-
-            </a>
-`;
-
-    main.insertAdjacentElement("afterbegin", navBar);
-})
+    main.insertAdjacentElement("afterbegin", navBar.cloneNode(true));
+});
